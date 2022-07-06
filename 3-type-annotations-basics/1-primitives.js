c872946269ab3d65405e4343005400240619c0fe
thijsBoet <m.boet@chello.nl>
2022-07-06 03:01:24 +0200

    learning primitive types in TS

---
+++ 3-type-annotations-basics/1-primitives.js
@@ -0,0 +1,6 @@
+var actor = 'Al Pacino';
+var born = 1940;
+var isAlive = true;
+actor.toUpperCase();
+var currentAge = new Date().getFullYear() - born;
+isAlive = false;
---
+++ 3-type-annotations-basics/1-primitives.ts
@@ -0,0 +1,7 @@
+const actor: string = 'Al Pacino';
+const born: number = 1940;
+let isAlive: boolean = true;
+
+actor.toUpperCase();
+const currentAge: number = new Date().getFullYear() - born;
+isAlive = false;
